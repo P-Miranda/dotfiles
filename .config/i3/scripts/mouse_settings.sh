23dd7f7cb69c0ba3e65268c6831fb4a50a26335f
P-Miranda <pedro.r.miranda@tecnico.ulisboa.pt>
2022-05-04 00:03:03 +0100

    feat(i3): Mouse sense, redshift - Add mouse sense script - Run mouse sense script and redshift on i3 start - Note: used lxappearence to set system theme

---
+++ .config/i3/scripts/mouse_settings.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+for id in $(xinput list | grep "Mouse" | grep -Po "(?<=id=)[^ \t]+")
+do
+	echo "updating xinput id=$id"
+    xinput --set-prop $id "libinput Accel Speed" -0.860000
+    xinput --set-prop $id "Coordinate Transformation Matrix" 1, 0, 0, 0, 1, 0, 0, 0, 1
+done
+
