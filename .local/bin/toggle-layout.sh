d060c0308c2ae81211af148d686f637fe52d29ff
P-Miranda <pedro.r.miranda@tecnico.ulisboa.pt>
2023-12-19 23:42:44 +0000

    fix(keyboard): alt key for level 3 - set alt key as modifier to access special characters [source](chttps://manpages.debian.org/bookworm/xkb-data/xkeyboard-config.7.en.html)

--- .local/bin/toggle-layout.sh
+++ .local/bin/toggle-layout.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # dmenu script to toggle between keyboard layouts
 options="setxkbmap -layout pt -option
-setxkbmap -layout us -variant intl"
+setxkbmap -layout us -variant intl -option lv3:alt_switch"
 
 set_kb_cmd=`echo -e "$options" |\
     dmenu -l 10`
